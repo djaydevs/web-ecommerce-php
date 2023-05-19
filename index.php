a226bbd1f72ba82cfb6a91a625f2a011620a078e
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-05-19 16:07:52 +0800

    added home page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./styles/styles.css ?v=<?php echo time(); ?>">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js" integrity="sha512-gmwBmiTVER57N3jYS3LinA9eb8aHrJua5iQD7yqYCKa5x6Jjc7VDVaEA0je0Lu0bP9j7tEjV3+1qUm6loO99Kw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>BURPGER</title>
+</head>
+<body>
+
+<div class="loading-page">
+    <svg id="loading-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M61.1 224C45 224 32 211 32 194.9c0-1.9 .2-3.7 .6-5.6C37.9 168.3 78.8 32 256 32s218.1 136.3 223.4 157.3c.5 1.9 .6 3.7 .6 5.6c0 16.1-13 29.1-29.1 29.1H61.1zM144 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm240 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32zM272 96a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zM16 304c0-26.5 21.5-48 48-48H448c26.5 0 48 21.5 48 48s-21.5 48-48 48H64c-26.5 0-48-21.5-48-48zm16 96c0-8.8 7.2-16 16-16H464c8.8 0 16 7.2 16 16v16c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V400z"/></svg>
+</div>
+
+<?php include 'components/main_header.php'; ?>
+
+<main>
+    <section class="hero | padding-block-900">
+        <div class="container">
+            <div class="even-columns">
+                <div class="flow">
+                <h1 class="fs-primary-heading fw-bold">Your home of the best delicious patties!</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore quos quas doloremque ipsum, aspernatur hic recusandae minima porro dolorem odio? Odio, animi! Excepturi incidunt deleniti culpa non et asperiores impedit.</p>
+                <button class="button">Order Now</button>
+                </div>
+                <div>
+                <img src="./assets/images/header1.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<script>
+    gsap.fromTo(
+        ".loading-page",
+        { opacity: 1 },
+        {
+            opacity: 0,
+            display: "none",
+            duration: 1.5,
+            delay: 2,
+        }
+    );
+</script>
+</body>
+</html>
