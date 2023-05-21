cfc342664593e3f371fccbc86986edf1a7f871e7
Alonzo Lucero <119100233+LucenzoX@users.noreply.github.com>
2023-05-21 22:24:18 +0800

    May Error(WIP)

--- checkout.php
+++ checkout.php
@@ -1,4 +1,5 @@
 <?php 
+   
     include 'components/connection.php';
 
     session_start();
@@ -7,110 +8,123 @@
     $user_id = $_SESSION['user_id'];
     }else{
     $user_id = '';
-    header('location:home.php');
+    header('location:index.php');
     };
 
+    if (isset($_POST['submit'])) {
+      $name = $_POST['name'];
+      $name = filter_var($name, FILTER_SANITIZE_STRING);
+      $number = $_POST['number'];
+      $number = filter_var($number, FILTER_SANITIZE_STRING);
+      $email = $_POST['email'];
+      $email = filter_var($email, FILTER_SANITIZE_STRING);
+      $method = $_POST['method'];
+      $method = filter_var($method, FILTER_SANITIZE_STRING);
+      $address = $_POST['address'];
+      $address = filter_var($address, FILTER_SANITIZE_STRING);
+      $total_products = $_POST['total_products'];
+      $total_price = $_POST['total_price'];
+  
+      $check_cart = $conn->prepare("SELECT COUNT(*) FROM `cart` WHERE user_id = ?");
+      $check_cart->execute([$user_id]);
+      $cart_items_count = $check_cart->fetchColumn();
+  
+      if ($cart_items_count > 0) {
+          if ($address == '') {
+              $message[] = 'please add your address!';
+          } else {
+              $insert_order = $conn->prepare("INSERT INTO `orders`(user_id, name, number, email, method, address, total_products, total_price) VALUES(?,?,?,?,?,?,?,?)");
+              $insert_order->execute([$user_id, $name, $number, $email, $method, $address, $total_products, $total_price]);
+  
+              $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+              $delete_cart->execute([$user_id]);
+  
+              $message[] = 'order placed successfully!';
+          }
+      } else {
+          $message[] = 'your cart is empty';
+      }
+  }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./styles/styles.css ?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <script src="https://kit.fontawesome.com/8eb0534a39.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js" integrity="sha512-gmwBmiTVER57N3jYS3LinA9eb8aHrJua5iQD7yqYCKa5x6Jjc7VDVaEA0je0Lu0bP9j7tEjV3+1qUm6loO99Kw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>BURPGER</title>
 </head>
+
 <body>
+   <?php include 'components/loading.php'; ?>
 
-<div class="loading-page">
-    <svg id="loading-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M61.1 224C45 224 32 211 32 194.9c0-1.9 .2-3.7 .6-5.6C37.9 168.3 78.8 32 256 32s218.1 136.3 223.4 157.3c.5 1.9 .6 3.7 .6 5.6c0 16.1-13 29.1-29.1 29.1H61.1zM144 128a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zm240 16a16 16 0 1 0 0-32 16 16 0 1 0 0 32zM272 96a16 16 0 1 0 -32 0 16 16 0 1 0 32 0zM16 304c0-26.5 21.5-48 48-48H448c26.5 0 48 21.5 48 48s-21.5 48-48 48H64c-26.5 0-48-21.5-48-48zm16 96c0-8.8 7.2-16 16-16H464c8.8 0 16 7.2 16 16v16c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V400z"/></svg>
-</div>
+   <?php include 'components/main_header.php'; ?>
 
-<?php include 'components/main_header.php'?>
+   <section class="checkout">
 
-<script src="./js/script.js"></script>
-<script>
-    gsap.fromTo(
-        ".loading-page",
-        { opacity: 1 },
-        {
-            opacity: 0,
-            display: "none",
-            duration: 1.5,
-            delay: 2,
-        }
-    );
-</script>
+      <form action="" method="post">
 
-<div class="heading">
-   <h3>checkout</h3>
-   <p><a href="home.php">home</a> <span> / checkout</span></p>
-</div>
+         <div class="cart-items">
+         <h3>cart items</h3>
+            <?php
+                  $grand_total = 0;
+                  $cart_items[] ='';
+                  $display = "SELECT * FROM cart WHERE user_id = '$user_id'";
+                  $output = mysqli_query($conn, $display);
+                  if(mysqli_num_rows($output) > 0){
+                     while ($row = $output->fetch_assoc()) {
+                        $cart_items[] = $row['name'].' ('.$row['price'].' x '. $row['quantity'].') - ';
+                        $total_products = implode($cart_items);
+                        $grand_total += ($row['price'] * $row['quantity']);
+               ?>
+               <p><span class="name"><?= $row['name']; ?></span><span class="price">$<?= $row['price']; ?> x <?= $row['quantity']; ?></span></p>
+               <?php
+                     }
+                  }else{
+                     echo '<p class="empty">your cart is empty</p>';
+                  }
+               ?>
+               <p class="grand-total"><span class="name">grand total :</span><span class="price">$<?= $grand_total; ?></span></p>
+               <a href="cart.php" class="btn">view cart</a>
+         </div>
 
-<section class="checkout">
+         <input type="hidden" name="total_products" value="<?= $total_products; ?>">
+         <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
+         <input type="hidden" name="name" value="<?= $fetch_profile['name'] ?>">
+         <input type="hidden" name="number" value="<?= $fetch_profile['number'] ?>">
+         <input type="hidden" name="email" value="<?= $fetch_profile['email'] ?>">
+         <input type="hidden" name="address" value="<?= $fetch_profile['address'] ?>">
 
-   <h1 class="title">order summary</h1>
+         <div class="user-info">
+         <h3>your info</h3>
+         <p><i class="fas fa-user"></i><span><?= $fetch_profile['name'] ?></span></p>
+         <p><i class="fas fa-phone"></i><span><?= $fetch_profile['number'] ?></span></p>
+         <p><i class="fas fa-envelope"></i><span><?= $fetch_profile['email'] ?></span></p>
+         <a href="user_profile_update_profile.php" class="btn">update info</a>
+         <h3>delivery address</h3>
+         <p><i class="fas fa-map-marker-alt"></i><span><?php if($fetch_profile['address'] == ''){echo 'please enter your address..';}else{echo $fetch_profile['address'];}?></span></p>
+         <a href="user_profile_update_address.php" class="btn">update address</a>
+         <select name="method" class="box" required>
+            <option value="" disabled selected>select payment method --</option>
+            <option value="cash on delivery">cash on delivery</option>
+            <option value="credit card">credit card</option>
+            <option value="paytm">paytm</option>
+            <option value="paypal">paypal</option>
+         </select>   
+         <input type="submit" value="place order" class="btn <?php if($fetch_profile['address'] == ''){echo 'disabled';} ?>" style="width:100%; background:var(--red); color:var(--white);" name="submit">
+      </div>
+   </form>
+   </section> 
 
-<form action="" method="post">
-
-   <div class="cart-items">
-      <h3>cart items</h3>
-      <?php
-         $grand_total = 0;
-         $cart_items[] = '';
-         $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ?");
-         $select_cart->execute([$user_id]);
-         if($select_cart->rowCount() > 0){
-            while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
-               $cart_items[] = $fetch_cart['name'].' ('.$fetch_cart['price'].' x '. $fetch_cart['quantity'].') - ';
-               $total_products = implode($cart_items);
-               $grand_total += ($fetch_cart['price'] * $fetch_cart['quantity']);
-      ?>
-      <p><span class="name"><?= $fetch_cart['name']; ?></span><span class="price">$<?= $fetch_cart['price']; ?> x <?= $fetch_cart['quantity']; ?></span></p>
-      <?php
-            }
-         }else{
-            echo '<p class="empty">your cart is empty!</p>';
-         }
-      ?>
-      <p class="grand-total"><span class="name">grand total :</span><span class="price">$<?= $grand_total; ?></span></p>
-      <a href="cart.php" class="btn">veiw cart</a>
-   </div>
-
-   <input type="hidden" name="total_products" value="<?= $total_products; ?>">
-   <input type="hidden" name="total_price" value="<?= $grand_total; ?>" value="">
-   <input type="hidden" name="name" value="<?= $fetch_profile['name'] ?>">
-   <input type="hidden" name="number" value="<?= $fetch_profile['number'] ?>">
-   <input type="hidden" name="email" value="<?= $fetch_profile['email'] ?>">
-   <input type="hidden" name="address" value="<?= $fetch_profile['address'] ?>">
-
-   <div class="user-info">
-      <h3>your info</h3>
-      <p><i class="fas fa-user"></i><span><?= $fetch_profile['name'] ?></span></p>
-      <p><i class="fas fa-phone"></i><span><?= $fetch_profile['number'] ?></span></p>
-      <p><i class="fas fa-envelope"></i><span><?= $fetch_profile['email'] ?></span></p>
-      <a href="update_profile.php" class="btn">update info</a>
-      <h3>delivery address</h3>
-      <p><i class="fas fa-map-marker-alt"></i><span><?php if($fetch_profile['address'] == ''){echo 'please enter your address';}else{echo $fetch_profile['address'];} ?></span></p>
-      <a href="update_address.php" class="btn">update address</a>
-      <select name="method" class="box" required>
-         <option value="" disabled selected>select payment method --</option>
-         <option value="cash on delivery">cash on delivery</option>
-         <option value="credit card">credit card</option>
-         <option value="paytm">paytm</option>
-         <option value="paypal">paypal</option>
-      </select>
-      <input type="submit" value="place order" class="btn <?php if($fetch_profile['address'] == ''){echo 'disabled';} ?>" style="width:100%; background:var(--red); color:var(--white);" name="submit">
-   </div>
-
-</form>
-   
-</section>
-
-</body>
-</html>
+   <script src="./js/main-container.js"></script>
+   <script src="./js/script.js"></script>
+</body>  
+</html> 
