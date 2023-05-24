3c0a090d8ce950274ac37b73099fa6d1cfe6d34c
martcrosoft <119098453+martcrosoft@users.noreply.github.com>
2023-05-24 14:54:46 +0800

    sana last na to

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,8 @@
 <?php 
+
+     if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
     include '../components/connection.php';
 
     $admin_id = $_SESSION['admin_id'];
--- admin/products.php
+++ admin/products.php
@@ -1,4 +1,8 @@
 <?php
+
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 error_reporting(0);
 
 include '../components/connection.php';
