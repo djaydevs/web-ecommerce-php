665dd869a5b4b7416006119eeb3668a70613e59d
Talh <ralphtaoc071@gmail.com>
2023-05-24 09:19:55 +0700

    about_revision1

--- about.php
+++ about.php
@@ -123,8 +123,8 @@ include 'components/add_to_cart.php';
 
             <div class="row">
                 <div class="col">
-                    <img src="assets/images/burger.png" alt="" class="image">
-                    <h3 class="title">BURGER</h3>
+                    <img src="assets/images/cat-combo.png" alt="" class="image">
+                    <h3 class="title">BURPGER</h3>
                 </div>
                 <div class="col">
                     <div class="flex">
